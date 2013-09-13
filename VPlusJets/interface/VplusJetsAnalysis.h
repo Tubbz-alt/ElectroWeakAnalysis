776da0bb61769f3790a8c6baffbbb76c5a5c7e68
zixu <zijun.xu@cern.ch>
2013-09-13 11:56:51 -0500

    add GEN, PF, PFCHS AK5 Jet

--- VPlusJets/interface/VplusJetsAnalysis.h
+++ VPlusJets/interface/VplusJetsAnalysis.h
@@ -98,7 +98,8 @@ namespace ewk
     std::auto_ptr<ewk::JetTreeFiller> CorrectedPFJetFiller;
     std::auto_ptr<ewk::JetTreeFiller> CorrectedPFJetFillerVBFTag; //For VBF Tag Jets
 
-    std::auto_ptr<ewk::GroomedJetFiller> AK5groomedJetFiller;
+    std::auto_ptr<ewk::GroomedJetFiller> AK5groomedJetFiller_PF;
+    std::auto_ptr<ewk::GroomedJetFiller> AK5groomedJetFiller_PFCHS;
     std::auto_ptr<ewk::GroomedJetFiller> AK8groomedJetFiller;
     std::auto_ptr<ewk::GroomedJetFiller> AK12groomedJetFiller;
     std::auto_ptr<ewk::GroomedJetFiller> CA8groomedJetFiller;
