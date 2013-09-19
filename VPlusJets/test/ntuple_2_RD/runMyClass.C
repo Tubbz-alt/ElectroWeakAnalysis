54e6e945b996c3babd687cd2647e01be6666651c
zixu <zijun.xu@cern.ch>
2013-09-18 22:28:28 -0500

    add some plots, change reco/gen jet matching

--- VPlusJets/test/ntuple_2_RD/runMyClass.C
+++ VPlusJets/test/ntuple_2_RD/runMyClass.C
@@ -1,6 +1,7 @@
 void runMyClass() {
 	gROOT->LoadMacro("MyClass.C");
-	TFile *file1 =new TFile("zmumujetsanalysisntuple.root");
+	//TFile *file1 =new TFile("zmumujetsanalysisntuple.root");
+	TFile *file1 =new TFile("full_zmumujetsanalysisntuple.root");
 
 
 	TFile *fout = new TFile("out.root", "RECREATE");
