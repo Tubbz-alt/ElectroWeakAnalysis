0cb00c60ef0abb52853042a2b194fdad55362400
zixu <zijun.xu@cern.ch>
2013-10-22 14:49:19 -0500

    add NSubjettinessRatio

--- VPlusJets/interface/ExampleShapes.hh
+++ VPlusJets/interface/ExampleShapes.hh
@@ -24,6 +24,8 @@
 
 #include "fastjet/FunctionOfPseudoJet.hh"
 #include "ShapeWithPartition.hh"
+#include "ShapeWithComponents.hh"
+#include "JetSubstructure/SubstructureTools/interface/JetSubstructureTools.h"
 
 //FASTJET_BEGIN_NAMESPACE
 
@@ -188,6 +190,64 @@ public:
 
 protected:
   const unsigned int _N;
+};
+
+/*
+class NSubjettinessRatio : public ShapeWithComponents{
+	public:
+		NSubjettinessRatio(int N) : _N(N){ assert(_N>1); }
+		virtual std::string description() const{ return "N-subjettiness ratio from components";}
+
+		virtual unsigned int n_components() const { return 2;}
+
+		virtual std::vector<double> components(const fastjet::PseudoJet &jet) const{
+			std::vector<double> comp(n_components());
+			comp[0] = NSubjettinessNumerator(_N)(jet);
+			comp[1] = NSubjettinessNumerator(_N-1)(jet);
+			return comp;
+		}
+
+		virtual double result_from_components(const std::vector <double> &components) const{
+			return components[0]/components[1];
+		}
+
+		virtual FunctionOfPseudoJet<double> * component_shape(unsigned int index) const{
+			return new NSubjettinessNumerator(_N-index);
+		}
+
+	protected:
+		const int _N;
+};*/
+class NSubjettinessRatio : public ShapeWithComponents{
+	public:
+		NSubjettinessRatio(int N, Njettiness::AxesMode in_mode, double in_beta, double in_R0, double in_Rcut) : _N(N), _mode(in_mode), _beta(in_beta), _R0(in_R0), _Rcut(in_Rcut) { assert(_N>1); }
+		virtual std::string description() const{ return "N-subjettiness ratio from components";}
+
+		virtual unsigned int n_components() const { return 2;}
+
+		virtual std::vector<double> components(const fastjet::PseudoJet &jet) const{
+			std::vector<double> comp(n_components());
+			comp[0] = fastjet::Nsubjettiness(_N  , _mode, _beta, _R0, _Rcut)(jet);
+			comp[1] = fastjet::Nsubjettiness(_N-1, _mode, _beta, _R0, _Rcut)(jet);
+
+			return comp;
+		}
+
+		virtual double result_from_components(const std::vector <double> &components) const{
+			return components[0]/components[1];
+		}
+
+		virtual FunctionOfPseudoJet<double> * component_shape(unsigned int index) const{
+			return new fastjet::Nsubjettiness(_N-index, _mode, _beta, _R0, _Rcut);
+		}
+
+	protected:
+		const int _N;
+		const Njettiness::AxesMode _mode;
+		const double _beta;
+		const double _R0;
+		const double _Rcut;
+
 };
 
 //} // namespace contrib
