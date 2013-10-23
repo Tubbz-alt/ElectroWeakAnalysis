57ded7e8678e2151d3dbcad35cb5334dd9d48438
zixu <zijun.xu@cern.ch>
2013-10-23 16:59:15 -0500

    add jetlable and pflable

--- VPlusJets/test/ntuple_2_RD/runMyClass.C
+++ VPlusJets/test/ntuple_2_RD/runMyClass.C
@@ -1,6 +1,11 @@
-void runMyClass( bool isBoosted=0 ) {
+void runMyClass( bool isBoosted=1 ) {
+	TString jetlable="AK8";//AK5
+	TString pflable="PF";//PFCHS
+	TString boostedlable;
+	if(isBoosted)boostedlable="boosted";
+	else boostedlable="unboosted";	
+	
 	gROOT->LoadMacro("MyClass.C+");
-
 	gROOT->LoadMacro("tdrstyle.C");
 	setTDRStyle(); //plotting style
 
@@ -10,10 +15,7 @@ void runMyClass( bool isBoosted=0 ) {
 	dir.ReplaceAll("/./","/");
 	TDatime dt;
 
-	TString boostedlable;
-	if(isBoosted)boostedlable="boosted";
-	else boostedlable="unboosted";
-	char* plot_Dir_DateTime=Form("%sPlots/%i_%i_%i/%s_%i",dir.Data(), dt.GetYear(), dt.GetMonth(), dt.GetDay(), boostedlable.Data(), dt.GetTime());
+	char* plot_Dir_DateTime=Form("%sPlots/%i_%i_%i/%s_%s_%s_%i",dir.Data(), dt.GetYear(), dt.GetMonth(), dt.GetDay(), boostedlable.Data(),jetlable.Data(),pflable.Data(), dt.GetTime());
 	cout<<plot_Dir_DateTime<<endl;
 	gSystem->mkdir(plot_Dir_DateTime, 1);
 
@@ -21,22 +23,20 @@ void runMyClass( bool isBoosted=0 ) {
 
 	TFile *file1;
 	if (isBoosted){
-		file1 = new TFile("full_boost_oct3_zmumujetsanalysisntuple.root");
+		//file1 = new TFile("full_boost_oct3_zmumujetsanalysisntuple.root");
+		file1 = new TFile("boosted_full_v4_zmumujetsanalysisntuple.root");
 	}else{
-		file1 = new TFile("unboosted_full_zmumujetsanalysisntuple.root");
+		//file1 = new TFile("unboosted_full_zmumujetsanalysisntuple.root");
+		//file1 = new TFile("zmumujetsanalysisntuple.root");
+		file1 = new TFile("unboosted_full_v1_zmumujetsanalysisntuple.root");
 	}
 
 	TFile *fout = new TFile(Form("%s/out_%s.root", plot_Dir_DateTime, boostedlable.Data()), "RECREATE");
 	TTree *tree1 = (TTree*) file1->Get("ZJet");
 
+
 	MyClass* analyzor;
-	if (isBoosted){
-		cout<<"boosted Z analysis"<<endl;
-		analyzor =  new MyClass(tree1,"AK8","PF", isBoosted, plot_Dir_DateTime);
-	}else{
-		cout<<"unboosted Z analysis"<<endl;
-		analyzor =  new MyClass(tree1,"AK5","PF", isBoosted, plot_Dir_DateTime);
-	}
+	analyzor =  new MyClass(tree1,jetlable.Data(),pflable.Data(), isBoosted, plot_Dir_DateTime);
 	analyzor->Loop();
 
 	fout->Write();
