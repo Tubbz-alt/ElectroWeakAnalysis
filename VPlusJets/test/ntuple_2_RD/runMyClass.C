68560cf242b611b25d99635ac75334b3899a70fe
zixu <zijun.xu@cern.ch>
2013-11-25 10:15:22 -0600

    add Dijet

--- VPlusJets/test/ntuple_2_RD/runMyClass.C
+++ VPlusJets/test/ntuple_2_RD/runMyClass.C
@@ -1,14 +1,27 @@
-void runMyClass( bool isBoosted=1 ) {
-	TString jetlable="AK5";//AK5
-	TString pflable="PF";//PFCHS
-	TString boostedlable;
-	if(isBoosted)boostedlable="boosted";
-	else boostedlable="unboosted";	
-	
+void runMyClass() {
 	gROOT->LoadMacro("MyClass.cc+");
 	gROOT->LoadMacro("tdrstyle.C");
 	setTDRStyle(); //plotting style
 
+	runMyClass_signle(0, "AK5", "PF");
+	/*runMyClass_signle(0, "AK5", "PFCHS");
+	runMyClass_signle(0, "AK8", "PF");
+	runMyClass_signle(0, "AK8", "PFCHS");
+
+	runMyClass_signle(1, "AK5", "PF");
+	runMyClass_signle(1, "AK5", "PFCHS");
+	runMyClass_signle(1, "AK8", "PF");
+	runMyClass_signle(1, "AK8", "PFCHS");*/
+}
+
+void runMyClass_signle( bool isBoosted, TString jetlable, TString pflable) {
+
+	TString final_state="Dijets";//
+	//TString final_state="ZJet";//
+
+	TString boostedlable;
+	if(isBoosted)boostedlable="boosted";
+	else boostedlable="unboosted";	
 	// mkdir plots fold accoding time
 	TString dir = gSystem->UnixPathName(gInterpreter->GetCurrentMacroName());
 	dir.ReplaceAll("runMyClass.C","");
@@ -19,27 +32,37 @@ void runMyClass( bool isBoosted=1 ) {
 	cout<<plot_Dir_DateTime<<endl;
 	gSystem->mkdir(plot_Dir_DateTime, 1);
 
-
-
 	TFile *file1;
-	if (isBoosted){
-		//file1 = new TFile("full_boost_oct3_zmumujetsanalysisntuple.root");
-		file1 = new TFile("boosted_full_v6_zmumujetsanalysisntuple.root");
-	}else{
-		//file1 = new TFile("unboosted_full_zmumujetsanalysisntuple.root");
-		//file1 = new TFile("zmumujetsanalysisntuple.root");
-		file1 = new TFile("unboosted_full_v6_zmumujetsanalysisntuple.root");
-	}
+	TTree *tree1;
 
+	if (final_state.Contains("ZJet")){
+		if (isBoosted){
+			file1 = new TFile("boosted_full_v7_zmumujetsanalysisntuple.root");
+		}else{
+			//file1 = new TFile("unboosted_full_v7_zmumujetsanalysisntuple.root");
+			//file1 = new TFile("full_zmumujetsanalysisntuple_highPU.root");
+			file1 = new TFile("dijetsanalysisntuple_1000.root");
+		}
+		tree1 = (TTree*) file1->Get("ZJet");
+
+	}else if (final_state.Contains("Dijets")) {
+
+		file1 = new TFile("dijetsanalysisntuple_1000.root");
+		tree1 = (TTree*) file1->Get("Dijets");
+
+	}else{
+		cout<<"Wrong final state: "<<final_state<<endl;
+	}
 	TFile *fout = new TFile(Form("%s/out_%s.root", plot_Dir_DateTime, boostedlable.Data()), "RECREATE");
-	TTree *tree1 = (TTree*) file1->Get("ZJet");
 
 
 	MyClass* analyzor;
-	analyzor =  new MyClass(tree1,jetlable.Data(),pflable.Data(), isBoosted, plot_Dir_DateTime);
+	cout<<"final_state="<<final_state.Data()<<endl;
+	analyzor =  new MyClass(tree1, final_state.Data(), jetlable.Data(), pflable.Data(), isBoosted, plot_Dir_DateTime);
 	analyzor->Loop();
 
 	fout->Write();
 	fout->Close();
+
 }
 
